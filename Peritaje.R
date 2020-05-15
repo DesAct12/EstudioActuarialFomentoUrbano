807c2620b49ef2e4448c9c91f89eda34ae59a42d
DesAct12 <isaac.actuario@gmail.com>
2020-05-15 00:13:08 -0600

    Se inicializa el cálculo de la distribución de ventas inmoviliarias.

---
+++ Peritaje.R
@@ -0,0 +1,19 @@
+# Fomento Urbano S.A.
+#                                                                                            
+#                                   Estudio Actuarial de Daños y Perjuicios
+#
+# Autores: Laura Campos 
+#          Isaac Z. Arias
+#
+# En este documento se realiza el cálculo por daños y perjuicios para la firma Fomento Urbano S.A. por la no no ejecución 
+# de un contrato de promesa por compra y venta.
+
+# Paquetes utilizados:
+library(dplyr)
+library(tidyr)
+
+
+# Carga de Datos:
+
+# Insumos para distribición de ventas:
+Insumo <- read.xlsx("Reservas_Quarzo_Novazul.xlsx")
