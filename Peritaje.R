7be2bdd777430adbca26ac1b2a9a1e8c53a26156
Laura Campos Quesada <lau.cq23@hotmail.com>
2020-05-24 15:20:03 -0600

    El archivo Modelo Financiero Avante - Las Lajas contiene la información para el cálculo del flujo de ingresos y gastos. En Peritaje.R se agrega el cálculo del flujo esperado del proyecto.

--- Peritaje.R
+++ Peritaje.R
@@ -1,11 +1,11 @@
 # Fomento Urbano S.A.
 #                                                                                            
-#                                   Estudio Actuarial de Daños y Perjuicios
+#                                   Estudio Actuarial de Da?os y Perjuicios
 #
 # Autores: Laura Campos 
 #          Isaac Z. Arias
 #
-# En este documento se realiza el cálculo por daños y perjuicios para la firma Fomento Urbano S.A. por la no-ejecución 
+# En este documento se realiza el c?lculo por da?os y perjuicios para la firma Fomento Urbano S.A. por la no-ejecuci?n 
 # de un contrato de promesa compraventa.
 
 # Paquetes utilizados:
@@ -18,16 +18,16 @@ Sys.setlocale("LC_TIME", "Spanish")
 
 #------------------------------------------------------ Carga de Datos:
 
-# Insumos para distribición de ventas:
+# Insumos para distribici?n de ventas:
 VentasReales <- read.xlsx("Reservas_Quarzo_Novazul.xlsx")
 
-#------------------------------------------------------ Análisis Exploratorio:
+#------------------------------------------------------ An?lisis Exploratorio:
 
 # Cantidad por tipos de Casas:
 Cantidad <- VentasReales %>% group_by(Proy, Obra) %>% summarise(Cantidad = n()) %>% ungroup()
 Cantidad
 
-# Se mimetizan los tipos de casas de acuerdo a sus características:
+# Se mimetizan los tipos de casas de acuerdo a sus caracter?sticas:
 VentasReales <- VentasReales %>% 
   filter(!Obra%in% c("AMATISTA 2D", "AMATISTA 3D", "ALTADENA", "ALTADENA 2D", "ALTADENA 3D")) %>% 
   mutate(Obra = ifelse(Obra %in% c("AMBAR 2D", "OPALO"), "AMBAR",
@@ -45,28 +45,28 @@ VentasReales <- VentasReales %>%
 # Caracteristicas de los Datos:
 summary(VentasReales)
 
-# Distribución de ventas generales:
+# Distribuci?n de ventas generales:
 ggplot(VentasReales, aes(x = Num_Mes, color = Obra)) +
   geom_histogram(fill="white")
 
-#------------------------------------------------------ Distribución por Tipo de Casa:
+#------------------------------------------------------ Distribuci?n por Tipo de Casa:
 
 Dist_Tipo <- VentasReales %>% group_by(Obra) %>% summarise(Prob = n()) %>% ungroup() %>% mutate(Prob = Prob/sum(Prob))
 Dist_Tipo
 
-#------------------------------------------------------ Distribución de Frecuencia para cada Tipo de Casa:
+#------------------------------------------------------ Distribuci?n de Frecuencia para cada Tipo de Casa:
 
-#---------------------------- Tipo: Ámbar
+#---------------------------- Tipo: ?mbar
 
 # Filtramos:
 Dist_Ambar <- VentasReales %>% filter(Obra == "AMBAR")
 
-# Primera Visualización Distribución
+# Primera Visualizaci?n Distribuci?n
 ggplot(Dist_Ambar, aes(x=Num_Mes, color=Obra)) +
   geom_histogram(aes(y=..density..)) +
   geom_density(alpha=.2, fill="#FF6766") 
 
-# Distribución: lo ideal es un ancho de banda de 5 meses
+# Distribuci?n: lo ideal es un ancho de banda de 5 meses
 ggplot(Dist_Ambar, aes(x=Num_Mes, color=Obra)) +
   geom_histogram() +
   stat_bin(binwidth = 5)
@@ -91,12 +91,12 @@ Dist_Ambar
 # Filtramos:
 Dist_Citrino <- VentasReales %>% filter(Obra == "CITRINO")
 
-# Primera Visualización Distribución
+# Primera Visualizaci?n Distribuci?n
 ggplot(Dist_Citrino, aes(x=Num_Mes, color=Obra)) +
   geom_histogram(aes(y=..density..)) +
   geom_density(alpha=.2, fill="#FF6766") 
 
-# Distribución: lo ideal es un ancho de banda de 3 meses
+# Distribuci?n: lo ideal es un ancho de banda de 3 meses
 ggplot(Dist_Citrino, aes(x=Num_Mes, color=Obra)) +
   geom_histogram() +
   stat_bin(binwidth = 5)
@@ -122,12 +122,12 @@ Dist_Citrino
 # Filtramos:
 Dist_Citrino <- VentasReales %>% filter(Obra == "CITRINO")
 
-# Primera Visualización Distribución
+# Primera Visualizaci?n Distribuci?n
 ggplot(Dist_Citrino, aes(x=Num_Mes, color=Obra)) +
   geom_histogram(aes(y=..density..)) +
   geom_density(alpha=.2, fill="#FF6766") 
 
-# Distribución: lo ideal es un ancho de banda de 3 meses
+# Distribuci?n: lo ideal es un ancho de banda de 3 meses
 ggplot(Dist_Citrino, aes(x=Num_Mes, color=Obra)) +
   geom_histogram() +
   stat_bin(binwidth = 5)
@@ -153,12 +153,12 @@ Dist_Citrino
 # Filtramos:
 Dist_Novazul <- VentasReales %>% filter(Obra == "NOVAZUL")
 
-# Primera Visualización Distribución
+# Primera Visualizaci?n Distribuci?n
 ggplot(Dist_Novazul, aes(x=Num_Mes, color=Obra)) +
   geom_histogram(aes(y=..density..)) +
   geom_density(alpha=.2, fill="#FF6766") 
 
-# Distribución: lo ideal es un ancho de banda de 3 meses
+# Distribuci?n: lo ideal es un ancho de banda de 3 meses
 ggplot(Dist_Novazul, aes(x=Num_Mes, color=Obra)) +
   geom_histogram() +
   stat_bin(binwidth = 5)
@@ -178,3 +178,258 @@ Dist_Novazul <- Dist_Novazul %>% mutate(Num_Mes = ifelse(Num_Mes<6, 1,
 
 # Visualizamos:
 Dist_Novazul
+
+#------------------------------------------------------ Valor futuro del flujo:
+Flujo_Gasto<-read.xlsx("Modelo Financiero Avante - Las Lajas.xlsx",sheet = "Gasto",colNames = T,rowNames = T,skipEmptyRows = T)
+
+# Gastos Fijos
+
+Flujo_Gasto<-as.data.frame(t(Flujo_Gasto[,-1]))
+Flujo_Gasto<- Flujo_Gasto %>% mutate(Gasto_Fijo=apply(Flujo_Gasto,1,sum)) %>% mutate(Gasto_Fijo=Gasto_Fijo-`Conexiones Servicios Publicos`-`Bienes Inmuebles`,Gastos_Compartidos=G.Ingeniero+Seguridad+`Gastos Administrativos`+`Gastos de Ventas (comision y otros)`)
+
+Flujo_Gasto$Gasto_Fijo[7:50]<-Flujo_Gasto$Gasto_Fijo[7:50]-Flujo_Gasto$Gastos_Compartidos[7:50]
+Gasto_Fijo<-Flujo_Gasto$Gasto_Fijo[1:14]
+
+
+# Flujo esperado de gastos aleatorios
+Gastos_Aleatorios<-sum(Flujo_Gasto$Gastos_Compartidos[7:50],Flujo_Gasto$`Conexiones Servicios Publicos`,Flujo_Gasto$`Bienes Inmuebles`)
+Dist_Ambar_2<-Dist_Ambar %>% mutate(Prob=Prob*Dist_Tipo$Prob[1])
+Dist_Citrino_2<-Dist_Citrino %>% mutate(Prob=Prob*Dist_Tipo$Prob[2])
+Dist_Novazul_2<-Dist_Novazul %>% mutate(Prob=Prob*Dist_Tipo$Prob[3])
+
+Dist_Ventas<-merge(Dist_Ambar_2,Dist_Citrino_2,by="Num_Mes",all.x = T,all.y = T)
+Dist_Ventas<-merge(Dist_Ventas,Dist_Novazul_2,by="Num_Mes",all.x = T,all.y = T)
+Dist_Ventas<-Dist_Ventas %>% mutate(Prob=ifelse(is.na(Prob),0,Prob),Prob.x=ifelse(is.na(Prob.x),0,Prob.x),Prob.y=ifelse(is.na(Prob.y),0,Prob.y))
+Dist_Ventas<-Dist_Ventas %>% mutate(Prob.total=Prob+Prob.x+Prob.y)
+Dist_Ventas<-rbind(rep(0,5),Dist_Ventas) %>% mutate(Num_Mes=1:9)
+Gasto_Aleatorios<-Gastos_Aleatorios*Dist_Ventas$Prob.total
+
+ ## Gastos por construccion de casas
+  Gastos_Construccion<-Dist_Ventas %>% mutate(Costos.a=123*Prob.x*42483,Costos.c=123*Prob.y*64640,Costos.n=123*Prob*53049.30) %>%
+    mutate(Costo_Total=Costos.a+Costos.c+Costos.n)
+  
+  Gasto_Construccion<-NULL
+  Gasto_Construccion[1]<-0
+  Gasto_Construccion[2]<-(5/8)*(Gastos_Construccion$Costo_Total[1])
+  for (j in 3:(length(Gastos_Construccion$Num_Mes)+1)) {
+    Gasto_Construccion[j]<-(5/8)*(Gastos_Construccion$Costo_Total[j-1])+(3/8)*(Gastos_Construccion$Costo_Total[j-2])
+    
+  }
+  Gasto_Construccion[j+1]<-(3/8)*(Gastos_Construccion$Costo_Total[j-1])
+  
+# Flujo esperado de ingresos
+  
+  Ingresos<-Dist_Ventas %>% mutate(Ingresos.a=123*Prob.x*106693,Ingresos.c=123*Prob.y*141326,Ingresos.n=123*Prob*122332) %>%
+    mutate(Ingreso_Total=Ingresos.a+Ingresos.c+Ingresos.n)
+  
+  Ingreso_Ventas<-NULL
+  Ingreso_Ventas[1]<-0
+  Ingreso_Ventas[2]<-(5/7)*0.2*(Ingresos$Ingreso_Total[1])
+  for (j in 3:(length(Ingresos$Num_Mes)+1)) {
+    Ingreso_Ventas[j]<-(5/7)*0.2*(Ingresos$Ingreso_Total[j-1])+((2/7)*0.2+0.8)*(Ingresos$Ingreso_Total[j-2])
+  }
+  Ingreso_Ventas[j+1]<-((2/7)*0.2+0.8)*(Ingresos$Ingreso_Total[j-1])
+  
+
+# Flujo esperado de gastos por deuda
+  
+Flujo_Gasto<-Flujo_Gasto %>% mutate(Gastos_Deuda=`Sistema Pluvial`+`Sistema Sanitario`+`Sistema Potable`+Pavimentos+`Obras Complementarias`+`Mejoras a media calle`+`Laguna de Retardo`+Muros+`Juegos Infantiles`+Otros+`Imprevistos(5%)`+Desfogue )  
+  
+
+Deuda<-as.data.frame(1:11)
+colnames(Deuda)<-"Num_Mes"
+Deuda <- Deuda %>% mutate(Desembolso=c(sum(Flujo_Gasto$Gastos_Deuda[1:5],500000),sum(Flujo_Gasto$Gastos_Deuda[6:10]),sum(Flujo_Gasto$Gastos_Deuda[11:14]),rep(0,8))) %>%
+  mutate(Desembolso=Desembolso+Gasto_Construccion, Ventas=Ingreso_Ventas) 
+
+Saldo<-NULL
+Saldo[1]<-Deuda$Desembolso[1]
+for(j in 2:length(Deuda$Num_Mes)){
+  Saldo[j]<-ifelse(Saldo[j-1]+Deuda$Desembolso[j]-Deuda$Ventas[j]*0.65>0,Saldo[j-1]+Deuda$Desembolso[j]-Deuda$Ventas[j]*0.65,ifelse(Saldo[j-1]+Deuda$Desembolso[j]-round((Saldo[j-1]+Deuda$Desembolso[j])/Deuda$Ventas[j],1)*Deuda$Ventas[j]>0,Saldo[j-1]+Deuda$Desembolso[j]-round((Saldo[j-1]+Deuda$Desembolso[j])/Deuda$Ventas[j],1)*Deuda$Ventas[j],Saldo[j-1]+Deuda$Desembolso[j]-(round((Saldo[j-1]+Deuda$Desembolso[j])/Deuda$Ventas[j],1)-1)*Deuda$Ventas[j]))
+}
+
+
+Deuda$Saldo<-Saldo
+Deuda<-rbind(Deuda,c(length(Deuda$Num_Mes)+1,0,0,0))
+Deuda<-Deuda %>% mutate(Interes=Saldo*0.095*5/12)
+
+  
+# Tasas (Valor futuro)
+
+TRI<-data.frame(PLazo=c(0.25,1,3,6,9,12,26,36,60),Tasa=c(0.0125,0.0265,0.0327,0.0411,0.0476,0.0603,0.0782,0.0803,0.0866))
+Flujo_Final<-Deuda %>% select(Num_Mes,Interes)
+Gasto_Fijo<-Gasto_Fijo<-c(sum(Gasto_Fijo[1:5]),sum(Gasto_Fijo[6:10]),sum(Gasto_Fijo[11:14]),rep(0,9))
+Gasto_Aleatorio<-c(Gasto_Aleatorios,rep(0,3))
+Gasto_Construccion<-c(Gasto_Construccion,0)
+Ingreso_Ventas<-c(Ingreso_Ventas,0)
+Flujo_Final<- cbind(Flujo_Final,Gasto_Fijo,Gasto_Aleatorio,Gasto_Construccion,Ingreso_Ventas)
+Flujo_Final<-Flujo_Final %>% mutate(Flujo=Ingreso_Ventas-Gasto_Fijo-Gasto_Aleatorio-Gasto_Construccion-Interes)
+
+Flujo_Final<-Flujo_Final %>% mutate(Tasa=approx(TRI$PLazo,TRI$Tasa,xout = abs(5*(1:12)-2*5))$y) %>%
+  mutate(Factor_acumulacion=case_when(Num_Mes>2 ~ (1+Tasa)^-1,
+                            Num_Mes<2 ~ 1+Tasa,
+                          Num_Mes==2 ~ 1)) %>%
+  mutate(Flujo_acumulado=Flujo*Factor_acumulacion)
+
+Ganancia_0<-sum(Flujo_Final$Flujo_acumulado)
+Ganancia_0
+
+# Covid 
+
+## Atraso de 5 meses
+
+# Flujo esperado de gastos aleatorios
+
+Dist_Ventas<-rbind(rep(0,5),Dist_Ventas) %>% mutate(Num_Mes=1:10)
+Gasto_Aleatorios<-Gastos_Aleatorios*Dist_Ventas$Prob.total
+
+## Gastos por construccion de casas
+Gastos_Construccion<-Dist_Ventas %>% mutate(Costos.a=123*Prob.x*42483,Costos.c=123*Prob.y*64640,Costos.n=123*Prob*53049.30) %>%
+  mutate(Costo_Total=Costos.a+Costos.c+Costos.n)
+
+Gasto_Construccion<-NULL
+Gasto_Construccion[1]<-0
+Gasto_Construccion[2]<-(5/8)*(Gastos_Construccion$Costo_Total[1])
+for (j in 3:(length(Gastos_Construccion$Num_Mes)+1)) {
+  Gasto_Construccion[j]<-(5/8)*(Gastos_Construccion$Costo_Total[j-1])+(3/8)*(Gastos_Construccion$Costo_Total[j-2])
+  
+}
+Gasto_Construccion[j+1]<-(3/8)*(Gastos_Construccion$Costo_Total[j-1])
+
+# Flujo esperado de ingresos
+
+Ingresos<-Dist_Ventas %>% mutate(Ingresos.a=123*Prob.x*106693,Ingresos.c=123*Prob.y*141326,Ingresos.n=123*Prob*122332) %>%
+  mutate(Ingreso_Total=Ingresos.a+Ingresos.c+Ingresos.n)
+
+Ingreso_Ventas<-NULL
+Ingreso_Ventas[1]<-0
+Ingreso_Ventas[2]<-(5/7)*0.2*(Ingresos$Ingreso_Total[1])
+for (j in 3:(length(Ingresos$Num_Mes)+1)) {
+  Ingreso_Ventas[j]<-(5/7)*0.2*(Ingresos$Ingreso_Total[j-1])+((2/7)*0.2+0.8)*(Ingresos$Ingreso_Total[j-2])
+}
+Ingreso_Ventas[j+1]<-((2/7)*0.2+0.8)*(Ingresos$Ingreso_Total[j-1])
+
+
+# Flujo esperado de gastos por deuda
+
+Flujo_Gasto<-Flujo_Gasto %>% mutate(Gastos_Deuda=`Sistema Pluvial`+`Sistema Sanitario`+`Sistema Potable`+Pavimentos+`Obras Complementarias`+`Mejoras a media calle`+`Laguna de Retardo`+Muros+`Juegos Infantiles`+Otros+`Imprevistos(5%)`+Desfogue )  
+
+
+Deuda<-as.data.frame(1:12)
+colnames(Deuda)<-"Num_Mes"
+Deuda <- Deuda %>% mutate(Desembolso=c(sum(Flujo_Gasto$Gastos_Deuda[1:5],500000),sum(Flujo_Gasto$Gastos_Deuda[6:10]),sum(Flujo_Gasto$Gastos_Deuda[11:14]),rep(0,9))) %>%
+  mutate(Desembolso=Desembolso+Gasto_Construccion, Ventas=Ingreso_Ventas) 
+
+Saldo<-NULL
+Saldo[1]<-Deuda$Desembolso[1]
+for(j in 2:length(Deuda$Num_Mes)){
+  Saldo[j]<-ifelse(Saldo[j-1]+Deuda$Desembolso[j]-Deuda$Ventas[j]*0.65>0,Saldo[j-1]+Deuda$Desembolso[j]-Deuda$Ventas[j]*0.65,ifelse(Saldo[j-1]+Deuda$Desembolso[j]-round((Saldo[j-1]+Deuda$Desembolso[j])/Deuda$Ventas[j],1)*Deuda$Ventas[j]>0,Saldo[j-1]+Deuda$Desembolso[j]-round((Saldo[j-1]+Deuda$Desembolso[j])/Deuda$Ventas[j],1)*Deuda$Ventas[j],Saldo[j-1]+Deuda$Desembolso[j]-(round((Saldo[j-1]+Deuda$Desembolso[j])/Deuda$Ventas[j],1)-1)*Deuda$Ventas[j]))
+}
+
+
+Deuda$Saldo<-Saldo
+Deuda<-rbind(Deuda,c(length(Deuda$Num_Mes)+1,0,0,0))
+Deuda<-Deuda %>% mutate(Interes=Saldo*0.095*5/12)
+
+
+
+# Tasas (Valor futuro)
+
+Flujo_Final<-Deuda %>% select(Num_Mes,Interes)
+Gasto_Fijo<-c(Gasto_Fijo,0)
+Gasto_Aleatorio<-c(Gasto_Aleatorios,rep(0,3))
+Gasto_Construccion<-c(Gasto_Construccion,0)
+Ingreso_Ventas<-c(Ingreso_Ventas,0)
+Flujo_Final<- cbind(Flujo_Final,Gasto_Fijo,Gasto_Aleatorio,Gasto_Construccion,Ingreso_Ventas)
+Flujo_Final<-Flujo_Final %>% mutate(Flujo=Ingreso_Ventas-Gasto_Fijo-Gasto_Aleatorio-Gasto_Construccion-Interes)
+
+Flujo_Final<-Flujo_Final %>% mutate(Tasa=approx(TRI$PLazo,TRI$Tasa,xout = abs(5*(1:13)-2*5))$y) %>%
+  mutate(Factor_acumulacion=case_when(Num_Mes>2 ~ (1+Tasa)^-1,
+                                      Num_Mes<2 ~ 1+Tasa,
+                                      Num_Mes==2 ~ 1)) %>%
+  mutate(Flujo_acumulado=Flujo*Factor_acumulacion)
+
+Ganancia_5<-sum(Flujo_Final$Flujo_acumulado)
+Ganancia_5
+
+
+## Atraso de 10 meses
+
+# Flujo esperado de gastos aleatorios
+
+Dist_Ventas<-rbind(rep(0,5),Dist_Ventas) %>% mutate(Num_Mes=1:11)
+Gasto_Aleatorios<-Gastos_Aleatorios*Dist_Ventas$Prob.total
+
+## Gastos por construccion de casas
+Gastos_Construccion<-Dist_Ventas %>% mutate(Costos.a=123*Prob.x*42483,Costos.c=123*Prob.y*64640,Costos.n=123*Prob*53049.30) %>%
+  mutate(Costo_Total=Costos.a+Costos.c+Costos.n)
+
+Gasto_Construccion<-NULL
+Gasto_Construccion[1]<-0
+Gasto_Construccion[2]<-(5/8)*(Gastos_Construccion$Costo_Total[1])
+for (j in 3:(length(Gastos_Construccion$Num_Mes)+1)) {
+  Gasto_Construccion[j]<-(5/8)*(Gastos_Construccion$Costo_Total[j-1])+(3/8)*(Gastos_Construccion$Costo_Total[j-2])
+  
+}
+Gasto_Construccion[j+1]<-(3/8)*(Gastos_Construccion$Costo_Total[j-1])
+
+# Flujo esperado de ingresos
+
+Ingresos<-Dist_Ventas %>% mutate(Ingresos.a=123*Prob.x*106693,Ingresos.c=123*Prob.y*141326,Ingresos.n=123*Prob*122332) %>%
+  mutate(Ingreso_Total=Ingresos.a+Ingresos.c+Ingresos.n)
+
+Ingreso_Ventas<-NULL
+Ingreso_Ventas[1]<-0
+Ingreso_Ventas[2]<-(5/7)*0.2*(Ingresos$Ingreso_Total[1])
+for (j in 3:(length(Ingresos$Num_Mes)+1)) {
+  Ingreso_Ventas[j]<-(5/7)*0.2*(Ingresos$Ingreso_Total[j-1])+((2/7)*0.2+0.8)*(Ingresos$Ingreso_Total[j-2])
+}
+Ingreso_Ventas[j+1]<-((2/7)*0.2+0.8)*(Ingresos$Ingreso_Total[j-1])
+
+
+# Flujo esperado de gastos por deuda
+
+Flujo_Gasto<-Flujo_Gasto %>% mutate(Gastos_Deuda=`Sistema Pluvial`+`Sistema Sanitario`+`Sistema Potable`+Pavimentos+`Obras Complementarias`+`Mejoras a media calle`+`Laguna de Retardo`+Muros+`Juegos Infantiles`+Otros+`Imprevistos(5%)`+Desfogue )  
+
+
+Deuda<-as.data.frame(1:13)
+colnames(Deuda)<-"Num_Mes"
+Deuda <- Deuda %>% mutate(Desembolso=c(sum(Flujo_Gasto$Gastos_Deuda[1:5],500000),sum(Flujo_Gasto$Gastos_Deuda[6:10]),sum(Flujo_Gasto$Gastos_Deuda[11:14]),rep(0,10))) %>%
+  mutate(Desembolso=Desembolso+Gasto_Construccion, Ventas=Ingreso_Ventas) 
+
+Saldo<-NULL
+Saldo[1]<-Deuda$Desembolso[1]
+for(j in 2:length(Deuda$Num_Mes)){
+  Saldo[j]<-ifelse(Saldo[j-1]+Deuda$Desembolso[j]-Deuda$Ventas[j]*0.65>0,Saldo[j-1]+Deuda$Desembolso[j]-Deuda$Ventas[j]*0.65,ifelse(Saldo[j-1]+Deuda$Desembolso[j]-round((Saldo[j-1]+Deuda$Desembolso[j])/Deuda$Ventas[j],1)*Deuda$Ventas[j]>0,Saldo[j-1]+Deuda$Desembolso[j]-round((Saldo[j-1]+Deuda$Desembolso[j])/Deuda$Ventas[j],1)*Deuda$Ventas[j],Saldo[j-1]+Deuda$Desembolso[j]-(round((Saldo[j-1]+Deuda$Desembolso[j])/Deuda$Ventas[j],1)-1)*Deuda$Ventas[j]))
+}
+
+
+Deuda$Saldo<-Saldo
+Deuda<-rbind(Deuda,c(length(Deuda$Num_Mes)+1,0,0,0))
+Deuda<-Deuda %>% mutate(Interes=Saldo*0.095*5/12)
+
+
+
+# Tasas (Valor futuro)
+
+Flujo_Final<-Deuda %>% select(Num_Mes,Interes)
+Gasto_Fijo<-c(Gasto_Fijo,0)
+Gasto_Aleatorio<-c(Gasto_Aleatorios,rep(0,3))
+Gasto_Construccion<-c(Gasto_Construccion,0)
+Ingreso_Ventas<-c(Ingreso_Ventas,0)
+Flujo_Final<- cbind(Flujo_Final,Gasto_Fijo,Gasto_Aleatorio,Gasto_Construccion,Ingreso_Ventas)
+Flujo_Final<-Flujo_Final %>% mutate(Flujo=Ingreso_Ventas-Gasto_Fijo-Gasto_Aleatorio-Gasto_Construccion-Interes)
+
+Flujo_Final<-Flujo_Final %>% mutate(Tasa=approx(TRI$PLazo,TRI$Tasa,xout = abs(5*(1:14)-2*5))$y) %>%
+  mutate(Factor_acumulacion=case_when(Num_Mes>2 ~ (1+Tasa)^-1,
+                                      Num_Mes<2 ~ 1+Tasa,
+                                      Num_Mes==2 ~ 1)) %>%
+  mutate(Flujo_acumulado=Flujo*Factor_acumulacion)
+
+Ganancia_10<-sum(Flujo_Final$Flujo_acumulado)
+Ganancia_10
+
+  
+
+###### GANANCIA ESPERADA FINAL
+GANANCIA_ESPERADA<-1/15*Ganancia_0+5/15*Ganancia_5+9/15*Ganancia_10
+GANANCIA_ESPERADA
